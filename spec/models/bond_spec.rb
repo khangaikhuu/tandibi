57121d6d9fb17a9444ecb79e81d9bb43b90a729e
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2021-10-09 23:36:31 +0800

    #7 [ADD] bond spec and fixing bond spec issues

--- spec/models/bond_spec.rb
+++ spec/models/bond_spec.rb
@@ -24,9 +24,11 @@ RSpec.describe Bond, type: :model do
   describe "#valid?" do
     it "should validate the state correctly" do
       friend = User.new
+      friend.save
       user = User.new
-      bond = Bond.new(user_id: user.id,
-                      friend_id: friend.id)
+      user.save
+      bond = Bond.new(user: user,
+                      friend: friend)
       
       expect(bond).not_to be_valid
 
@@ -36,4 +38,28 @@ RSpec.describe Bond, type: :model do
       end
     end
   end
+
+  describe "#save" do
+    context "when complete data is given" do
+      it "can be persisted" do
+        user = User.create email: "e1@example.org",
+                           first_name: "Edwin",
+                           username: "e1"
+        
+        friend = User.create email: "a1@example.org",
+                             first_name: "Adam",
+                             username: "a1"
+        
+        bond = Bond.new(
+          user: user,
+          friend: friend,
+          state: Bond::REQUESTING
+          )
+        bond.save
+        expect(bond).to be_persisted
+        expect(bond.user).to eq user
+        expect(bond.friend).to eq friend
+     end
+   end
+ end
 end
