b846d64aea404077ed5f5d40edaac4be9413054b
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2022-03-26 22:48:32 +0800

    [ADD] home controller, user and user spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :home#index
       expect(response).to have_http_status(:success)
     end
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,19 +2,24 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string
-#  first_name :string
-#  is_public  :boolean
-#  last_name  :string
-#  username   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  email                  :string
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  is_public              :boolean
+#  last_name              :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email     (email) UNIQUE
-#  index_users_on_username  (username) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 FactoryBot.define do
   factory :user do
@@ -23,6 +28,7 @@ FactoryBot.define do
     last_name { ["Soesanto", "Yamashita", "de Flaire"].sample }
     email { "#{SecureRandom.hex(4)}@example.org" }
     is_public { true }
+    encrypted_password { SecureRandom.hex(5) }
 
   end
 end
